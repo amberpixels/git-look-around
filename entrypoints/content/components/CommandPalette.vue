c6ef812f8445d41db009a1433f101f06e0e46810
Eugene <eugene@amberpixels.io>
2025-11-18 16:28:29 +0200

    style updates

--- entrypoints/content/components/CommandPalette.vue
+++ entrypoints/content/components/CommandPalette.vue
@@ -108,32 +108,32 @@
                     closed: item.state === 'closed',
                   }"
                 >
-                  <div class="repo-details-meta">
-                    <svg
-                      v-if="item.type === 'pr'"
-                      class="repo-details-icon pr"
-                      :class="item.state"
-                      viewBox="0 0 16 16"
-                      width="14"
-                      height="14"
-                    >
-                      <path
-                        d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"
-                      ></path>
-                    </svg>
-                    <svg
-                      v-else
-                      class="repo-details-icon issue"
-                      :class="item.state"
-                      viewBox="0 0 16 16"
-                      width="14"
-                      height="14"
-                    >
-                      <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
-                      <path
-                        d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"
-                      ></path>
-                    </svg>
+                  <svg
+                    v-if="item.type === 'pr'"
+                    class="repo-details-icon pr"
+                    :class="item.state"
+                    viewBox="0 0 16 16"
+                    width="14"
+                    height="14"
+                  >
+                    <path
+                      d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"
+                    ></path>
+                  </svg>
+                  <svg
+                    v-else
+                    class="repo-details-icon issue"
+                    :class="item.state"
+                    viewBox="0 0 16 16"
+                    width="14"
+                    height="14"
+                  >
+                    <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z"></path>
+                    <path
+                      d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Z"
+                    ></path>
+                  </svg>
+                  <div class="repo-details-content">
                     <span class="repo-details-number">#{{ item.number }}</span>
                     <img
                       :src="item.user.avatar_url"
@@ -141,10 +141,10 @@
                       :title="`Opened by @${item.user.login}`"
                       class="repo-details-avatar"
                     />
+                    <a :href="item.html_url" class="repo-details-link" @click="handleRepoClick">
+                      {{ item.title }}
+                    </a>
                   </div>
-                  <a :href="item.html_url" class="repo-details-link" @click="handleRepoClick">
-                    {{ item.title }}
-                  </a>
                 </li>
               </ul>
             </div>
@@ -1259,10 +1259,9 @@ defineExpose({
 }
 
 .repo-details-row {
-  display: grid;
-  grid-template-columns: auto 1fr;
-  gap: 8px;
+  display: flex;
   align-items: center;
+  gap: 6px;
   padding: 4px 12px;
   border: none;
   border-bottom: 1px solid #e1e4e8;
@@ -1287,13 +1286,6 @@ defineExpose({
   color: #8b949e;
 }
 
-.repo-details-meta {
-  display: flex;
-  align-items: center;
-  gap: 6px;
-  flex-shrink: 0;
-}
-
 .repo-details-icon {
   flex-shrink: 0;
   fill: currentColor;
@@ -1315,6 +1307,14 @@ defineExpose({
   color: #8b949e;
 }
 
+.repo-details-content {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  flex: 1;
+  min-width: 0;
+}
+
 .repo-details-number {
   font-size: 11px;
   font-weight: 600;
@@ -1323,6 +1323,7 @@ defineExpose({
   font-family: 'SF Mono', Monaco, 'Courier New', monospace;
   min-width: 42px;
   text-align: left;
+  flex-shrink: 0;
 }
 
 .repo-details-avatar {
@@ -1339,6 +1340,8 @@ defineExpose({
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+  flex: 1;
+  min-width: 0;
 }
 
 .repo-details-link:hover {
